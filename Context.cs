3e8a39b82880ec0b7d2ff9dececeafc4c84e61dd
track507 <136283573+track507@users.noreply.github.com>
2024-04-29 06:36:22 -0700

    ConcreteStateC
    Added ConcreteStateC, other 2 states will be implemented by group members

--- Context.cs
+++ Context.cs
@@ -3,19 +3,27 @@
 namespace ContextNamespace;
 class Context {
     private State currentState = null;
+    // On context update, call transition
     public Context(State state) {
         TransitionTo(state);
     }
+    // Transition handler
     public void TransitionTo(State state) {
         Console.WriteLine($"Context: Transition to {state.GetType().Name}.");
         currentState = state;
         currentState.SetContext(this);
     }
+    // First game
     public void Request1() {
         currentState.Handle1();
     }
+    // Second game
     public void Request2() {
         currentState.Handle2();
+    }
+    // Third game
+    public void Request3() {
+        currentState.Handle3();
     }
 }
 
--- State.cs
+++ State.cs
@@ -7,5 +7,22 @@ abstract class State {
         currentContext = context;
     }
     public abstract void Handle1();
-    public abstract void Handle2();   
+    public abstract void Handle2();
+    public abstract void Handle3();   
+}
+
+class ConcreteStateC : State {
+    public override void Handle1() {
+        // ConcreteStateA will be implemented by group member
+        Console.WriteLine("ConcretesStateC handles request 1");
+    }
+    public override void Handle2() {
+        // ConcreteStateB will be implemented by group member
+        Console.WriteLine("ConcreteStateC handles request 2");
+    }
+    public override void Handle3() {
+        Console.WriteLine("ConcreteStateC handles request 3");
+        Console.WriteLine("ConcreteStateC is changing states...");
+        currentContext.TransitionTo(new ConcreteStateA());
+    }
 }
